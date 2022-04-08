d74b2057a0c1901645810e43ed3c0946a5359236
Isabelola <96275763+Isabelola@users.noreply.github.com>
2022-04-08 11:39:25 -0400

    Add files via upload

--- DS_escola/cadastro_escola/MainWindow.xaml.cs
+++ DS_escola/cadastro_escola/MainWindow.xaml.cs
@@ -27,7 +27,48 @@ namespace cadastro_escola
 
         private void btCadastrar_Click(object sender, RoutedEventArgs e)
         {
-            MessageBox.Show("Escola Cadastrada!");
+            string nomeFantasia = txtNomeFantasia.Text;
+            string razaoSocial = txtRazaoSocial.Text;
+            string cnpj = txtCNPJ.Text;
+            string contato = txtContato.Text;
+            string email = txtEmail.Text;
+            string inscricaoEstad = txtInscEstadual.Text;
+            string responsavel = txtResponsavel.Text;
+            string cidade = txtCidade.Text;
+            string rua = txtRua.Text;
+            string bairro = txtBairro.Text;
+            string cep = txtCEP.Text;
+            string numero = txtNumero.Text;
+            string pontRef = txtReferencia.Text;
+            string uf = txtUF.Text;
+            string tipo = "Informação Vaga";
+            
+
+            if (rdTipoPublico.IsChecked == true)
+            {
+                tipo = rdTipoPublico.Content.ToString();
+            }
+            if (rdTipoPrivada.IsChecked == true)
+            {
+                tipo = rdTipoPrivada.Content.ToString();
+            }
+
+
+            MessageBox.Show("Nome Fantasia: " + nomeFantasia + Environment.NewLine +
+                "Razão Social: " + razaoSocial + Environment.NewLine +
+                "CNPJ: " + cnpj + Environment.NewLine +
+                "Contatol: " + contato + Environment.NewLine +
+                "E-mail: " + email + Environment.NewLine +
+                "Incrição estadual: " + inscricaoEstad + Environment.NewLine +
+                "Responsável: " + responsavel + Environment.NewLine + 
+                "Tipo: " + tipo + Environment.NewLine +
+                "Cidade: " + cidade + Environment.NewLine +
+                "Rua: " + rua + Environment.NewLine +
+                "Bairro: " + bairro + Environment.NewLine + 
+                "CEP: " + cep + Environment.NewLine +
+                "N°: " + numero + Environment.NewLine +
+                "Ponto de referência: " + pontRef + Environment.NewLine +
+                "UF: " + uf + Environment.NewLine);
         }
     }
 }
